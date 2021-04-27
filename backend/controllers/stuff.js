37746b63004610a12431516dc0e01da9404a8cec
Thibaut <herbert.thibaut@protonmail.com>
2021-04-27 14:20:05 -0400

    route likes dislikes add switch and cases

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -44,7 +44,37 @@ exports.like = (req, res, next) => {
 };
 */
 // test 4
-
+exports.likeSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            switch(req.body.like) {             // switch évalue l'expression fournie : like 
+                case 1: // utilisateur aime la sauce
+                    console.log("Le " + sauce.name + " j'aime");
+                    // identifiant de l'utilisateur doit être ajouté du tableau approprié 
+                    // en gardant une trace de ses préférences et en l'empêchant d'aimer la même sauce plusieurs fois
+                    // mettre à jour nbr total de j'aime
+                    break; 
+                    // break permet de s'assurer que seules les instructions associées à ce cas seront exécutées
+                    // si break n'est pas utilisée le programme continuera son exécution
+                    /// break empêche donc la répétition d'aimer la même sauce plusieurs fois ??
+                case 0: // utilisateur annule ce qu'il aime ou n'aime pas
+                    console.log("Le " + sauce.name + " 0 j'aime, 0 j'aime pas");
+                    // identifiant de l'utilisateur doit être supprimé du tableau approprié 
+                    // en gardant une trace de ses préférences et en l'empêchant de ne pas aimer ou d'aimer la même sauce plusieurs fois
+                    // mettre à jour nbr total de j'aime
+                    break;
+                case -1: // utilisateur n'aime la sauce
+                    console.log("Le " + sauce.name + " je n'aime pas");
+                    // identifiant de l'utilisateur doit être ajouté du tableau approprié
+                    // en gardant une trace de ses préférences et en l'empêchant de ne pas aimer la même sauce plusieurs fois
+                    // mettre à jour nbr total de je n'aime pas
+                    break;
+                default:
+                    console.log('Désolé, un problème a été détecté'); //Une clause exécutée si aucune correspondance n'est trouvée avec les clause case (et/ou s'il n'y a pas de break pour les clauses case précédentes)
+            }
+        })
+        .catch(error => res.status(400).json({ error }));
+};
 exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ?
     { 
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -7,7 +7,7 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
-//router.post('/:id/like', auth, multer, sauceCtrl.likeSauce);
+router.post('/:id/like', auth, multer, sauceCtrl.likeSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
